1154644fd244ba58d8c58e8d84a7973bb68642e8
banjoDevs <pableobenedick033@gmail.com>
2023-03-19 21:40:51 +0800

    update commit

--- src/App.js
+++ src/App.js
@@ -7,22 +7,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { data } from './data.js';
 
 function App() {
+const [countries, setCountries] = useState(data);
 const [search, setSearch] = useState('');
 
 
+
+  const resetCountries = () => {
+    setCountries(data);
+  };
   return (
     <div>
         <Container>
-          <h1>Countries</h1>
+          <h1 className='text-center mt-4'>Countries</h1>
           <Form>
-            <InputGroup>
+            <InputGroup className='my-3'>
               <Form.Control 
               onChange={(e) => setSearch(e.target.value)}
               placeholder='Search countries' />
             </InputGroup>
           </Form>
           <div>
-            <button></button>
+            <button className='btn btn-primary me-2' onClick={() => sortByName(true)}>Sort A-Z</button>
+            <button className='btn btn-primary me-2' onClick={() => sortByName(false)}>Sort Z-A</button>
+            <button className='btn btn-primary me-2' onClick={resetCountries}>Reset</button>
           </div>
           <Table>
             <thead>
