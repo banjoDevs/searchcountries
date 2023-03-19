1c664ac4653a1c78e18a033448bf570859849d86
banjoDevs <pableobenedick033@gmail.com>
2023-03-19 21:13:31 +0800

    update commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { data } from './data.js';
 
 function App() {
+
+
+
   return (
-    <div className="App">
-        Hello World
+    <div>
+        <Container>
+          <h1>Countries</h1>
+          <Form>
+            <Input>
+              <Form.Control 
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder='Search countries' />
+            </Input>
+          </Form>
+        </Container>
     </div>
   );
 }
